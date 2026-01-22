a2bf6799877f581bc268a769e99dfa01721613e1
hmtwins <justmohsan@gmail.com>
2026-01-22 13:29:53 -0500

    Update train.py

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ import os
 # 1. Create the environment
 env = NanoBotEnv()
 
-# 2. Define the AI Model (The Brain)
+# 2. Define the AI Model 
 # MlpPolicy = Multi-Layer Perceptron (Standard Neural Network)
 # verbose=1 allows us to see the training progress log
 model = PPO("MlpPolicy", env, verbose=1, learning_rate=0.0003)
@@ -15,11 +15,11 @@ print("--- STARTING TRAINING ---")
 print("The AI is now experimenting with magnetic fields...")
 
 # 3. Train the model
-# 50,000 timesteps is usually enough for this simple 2D physics
 model.learn(total_timesteps=200000)
 
 print("--- TRAINING COMPLETE ---")
 
 # 4. Save the brain
 model.save("nano_ai_brain")
+
 print("Model saved as nano_ai_brain.zip")
